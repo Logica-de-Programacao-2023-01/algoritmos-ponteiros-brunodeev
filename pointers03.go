a3d0ccc3ad8c6b551e2ddbe54a005b8575c325ec
Bruno César <79723563+brunodeev@users.noreply.github.com>
2023-06-25 12:22:16 -0300

    pointers03.go

---
+++ pointers03.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func inverterString(s *string) {
+	newS := strings.Split(*s, "")
+	*s = ""
+
+	for i := len(newS) - 1; i >= 0; i-- {
+		*s += newS[i]
+	}
+}
+
+func main() {
+	s := "Hello"
+	inverterString(&s)
+	fmt.Println(s)
+}
